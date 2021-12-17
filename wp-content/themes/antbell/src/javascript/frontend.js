9cb9d41aba6828601bdffb89dc88b258860902e4
Hasani Rogers <hasani.rogers@gmail.com>
2021-12-17 12:56:45 -0500

    more testing

--- wp-content/themes/antbell/src/javascript/frontend.js
+++ wp-content/themes/antbell/src/javascript/frontend.js
@@ -1,4 +1,4 @@
 import '@lrnwebcomponents/lrndesign-audio-player/lrndesign-audio-player.js';
-// import 'kemet-ui/dist/components/kemet-scroll-nav.js';
+import 'kemet-ui/dist/components/kemet-scroll-nav.js';
 import './scripts/blocks/page-background-frontend.js';
 import './components/abm-button/abm-button.js';
--- wp-content/themes/antbell/src/javascript/scripts/blocks/page-background-frontend.js
+++ wp-content/themes/antbell/src/javascript/scripts/blocks/page-background-frontend.js
@@ -3,13 +3,17 @@ const observer = new IntersectionObserver(
     entries.forEach(entry => {
       if (entry.intersectionRatio > 0.5) {
         entry.target.classList.add('page-background__container--visible');
+        console.log('add visible');
       } else {
         entry.target.classList.remove('page-background__container--visible');
+        console.log('remove visible');
       }
     });
   },
   { threshold: 0.5 }
 );
+
+console.log(observer);
 
 document.querySelectorAll('.page-background__container').forEach(container => {
   observer.observe(container);
