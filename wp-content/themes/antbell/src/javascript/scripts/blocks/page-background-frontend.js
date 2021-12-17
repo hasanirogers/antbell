35d1d39c54e36ae6bc289399b8c620ac0f16f911
Hasani Rogers <hasani.rogers@gmail.com>
2021-12-17 13:16:56 -0500

    adjusted intersectionRatio

--- wp-content/themes/antbell/src/javascript/scripts/blocks/page-background-frontend.js
+++ wp-content/themes/antbell/src/javascript/scripts/blocks/page-background-frontend.js
@@ -1,19 +1,15 @@
 const observer = new IntersectionObserver(
   (entries, observer) => {
     entries.forEach(entry => {
-      if (entry.intersectionRatio > 0.5) {
+      if (entry.intersectionRatio > 0.2) {
         entry.target.classList.add('page-background__container--visible');
-        console.log('add visible');
       } else {
         entry.target.classList.remove('page-background__container--visible');
-        console.log('remove visible');
       }
     });
   },
   { threshold: 0.5 }
 );
-
-console.log(observer);
 
 document.querySelectorAll('.page-background__container').forEach(container => {
   observer.observe(container);
